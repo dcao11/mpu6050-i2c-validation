eae4eea6e14c16ae83ba38eb08b81be6e57ad974
David <dcao11@hotmail.com>
2026-04-04 16:42:43 -0700

    Initial Commit

---
+++ arduino_src/mpu_driver/mpu_driver.ino
@@ -0,0 +1,213 @@
+#include <Wire.h>
+#include <stdint.h>
+
+// registers 
+const uint8_t ADDR_MPU6050 = 0x68;
+
+const uint8_t REG_PWR_MGMT_1 = 0x6B;
+const uint8_t REG_WHO_AM_I = 0x75;
+
+const uint8_t REG_ACCEL_XOUT = 0x3B; //[15:8], 0x3C is [7:0] likewise for the other accelerometer 
+const uint8_t REG_ACCEL_YOUT = 0x3D;
+const uint8_t REG_ACCEL_ZOUT = 0x3F;
+
+
+struct Result8 {
+    bool success;
+    uint8_t value;
+};
+
+struct Result16 {
+    bool success;
+    int16_t value;
+};
+
+struct BurstResult6 {
+    bool success;
+    uint8_t data[6];
+};
+
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial);
+  Wire.begin();
+
+  // Wake up the sensor
+  writeRegister(ADDR_MPU6050,REG_PWR_MGMT_1, 0x00);
+}
+
+void loop() {
+  if (Serial.available()){
+    String serial_input = Serial.readStringUntil('\n');
+    serial_input.trim();
+    if (serial_input =="READ_WHOAMI"){
+      Result8 RES_WHO_AM_I = readRegister8(ADDR_MPU6050,REG_WHO_AM_I);
+      if (RES_WHO_AM_I.success){
+        Serial.print("OK:WHO_AM_I:");
+        Serial.println(RES_WHO_AM_I.value);
+      }
+      else{
+        Serial.println("ERR:MPU_NOT_DETECTED");
+      }
+    }
+    else if(serial_input =="READ_ACCEL"){
+      BurstResult6 RES_ACCEL = readBurst6(ADDR_MPU6050, REG_ACCEL_XOUT);
+      if (RES_ACCEL.success){
+        int16_t RES_ACCEL_XOUT = (RES_ACCEL.data[0] << 8) | RES_ACCEL.data[1];
+        int16_t RES_ACCEL_YOUT = (RES_ACCEL.data[2] << 8) | RES_ACCEL.data[3];
+        int16_t RES_ACCEL_ZOUT = (RES_ACCEL.data[4] << 8) | RES_ACCEL.data[5];
+        Serial.print("OK:ACCEL:"); 
+        Serial.print(RES_ACCEL_XOUT);
+        Serial.print(":"); 
+        Serial.print(RES_ACCEL_YOUT);
+        Serial.print(":"); 
+        Serial.println(RES_ACCEL_ZOUT);
+      }
+      else if (serial_input =="READ_WHOAMI_BAD_ADDR"){
+        Result8 RES_WHO_AM_I = readRegister8(ADDR_MPU6050 + 1,REG_WHO_AM_I);
+        if (RES_WHO_AM_I.success){
+          Serial.print("OK:WHO_AM_I:");
+          Serial.println(RES_WHO_AM_I.value);
+        }
+        else{
+          Serial.println("ERR:MPU_NOT_DETECTED");
+        }
+      }
+      else{
+        Serial.println("ERROR:FAILED_READ_ACCEL");
+      }
+    }
+  }
+}
+
+
+
+
+// Reads a single 8-bit register from a specific address
+Result8 readRegister8(uint8_t device_addr, uint8_t reg) {
+  Result8 default_value = {false, 0};  // start with failed case
+
+  Wire.beginTransmission(device_addr);
+  Wire.write(reg);
+
+  if (Wire.endTransmission(false) != 0) {  // 0 = success
+    return default_value;
+  }
+
+  if (Wire.requestFrom(device_addr, 1) != 1) {  // expect 1 byte
+    return default_value;
+  }
+
+  uint8_t val = Wire.read();
+  return {true, val};
+}
+
+
+
+Result16 readRegister16(uint8_t device_addr, uint8_t reg){
+  Result16 default_value = {false, 0};  // start with failed case
+
+  Wire.beginTransmission(device_addr);
+  Wire.write(reg);
+  if (Wire.endTransmission(false) != 0) {  // 0 = success
+    return default_value;
+  }
+
+  if (Wire.requestFrom(device_addr, 2) != 2) {  // expect 1 byte
+    return default_value;
+  }
+
+  uint8_t MSB = Wire.read();
+  uint8_t LSB = Wire.read();
+  int16_t val = (int16_t)((MSB << 8)|LSB);
+  return {true, val};
+}
+
+
+
+// reading multiple register at once to reduce overhead 
+BurstResult6 readBurst6(uint8_t device_addr, uint8_t reg) {
+  BurstResult6 default_value = {false, {0, 0, 0, 0, 0, 0}};
+
+  Wire.beginTransmission(device_addr);
+  Wire.write(reg);
+
+  if (Wire.endTransmission(false) != 0) {
+    return default_value;
+  }
+
+  if (Wire.requestFrom(device_addr, 6) != 6) {
+    return default_value;
+  }
+
+  default_value.success = true;
+
+  for (uint8_t i = 0; i < 6; i++) {
+    default_value.data[i] = Wire.read();
+  }
+
+  return default_value;
+}
+
+
+
+// helper function to write to a specific 8 bit register
+void writeRegister(uint8_t device_addr, uint8_t reg, uint8_t val) {
+  Wire.beginTransmission(device_addr);
+  Wire.write(reg);
+  Wire.write(val);
+  Wire.endTransmission();
+}
+
+
+
+
+// old
+/*
+uint8_t readRegister8(uint8_t device_addr, uint8_t reg) {
+  Wire.beginTransmission(device_addr);
+  Wire.write(reg);
+  if (Wire.endTransmission(false) != 0){  // Repeated Start
+    return 0;
+  }
+  if (Wire.requestFrom(device_addr, 1)!= 1){
+    return 0xFF;
+  }
+  return Wire.read();
+}
+
+
+
+// read a 16 bit register from a specific address
+int16_t readRegister16(uint8_t device_addr, uint8_t reg){
+  Wire.beginTransmission(device_addr);
+  Wire.write(reg);
+  if (Wire.endTransmission(false) != 0){  // Repeated Start
+    return 0;
+  }
+  if (Wire.requestFrom(device_addr, 2) != 2){
+    return 0xFFFF;
+  } 
+  uint8_t MSB = Wire.read();
+  uint8_t LSB = Wire.read();
+  return (int16_t) ((MSB <<8) | LSB);
+}
+
+
+
+// reading multiple register at once to reduce overhead 
+void readBurst(uint8_t device_addr, uint8_t reg, uint8_t* buffer, uint8_t length) {
+  Wire.beginTransmission(device_addr);
+  Wire.write(reg);
+  if (Wire.endTransmission(false) != 0) return;
+  if (Wire.requestFrom(device_addr, length) != length) return;
+  for (uint8_t i = 0; i < length; i++) {
+    buffer[i] = Wire.read();
+  }
+}
+
+
+*/
+
+
---
+++ python_src/main.py
@@ -0,0 +1,94 @@
+import serial
+import time
+from datetime import datetime
+import csv
+import os
+
+#hello world
+
+PORT = "COM3"
+BAUD = 9600
+ser = serial.Serial('COM3', 9600, timeout=1)
+time.sleep(2)  # Arduino reset delay
+
+
+def evaluate_result(response: list[str]) -> str:
+    status, test = response[0:2]
+
+    if status == "ERR":
+        return "FAIL"
+
+    if test == "WHO_AM_I":
+        return "PASS" if response[2] == "104" else "FAIL"
+
+    if test == "ACCEL":
+        z = int(response[4])
+        return "PASS" if abs(z) > 14000 else "FAIL"
+
+    return "FAIL"
+
+
+def send_command(cmd) -> str:
+    ser.write((cmd+"\n").encode())
+    time.sleep(0.1) # give arduino time
+    response = ser.readline().decode().strip()
+    return response
+
+
+def get_precise_time() -> str:
+    # 1. Get the raw float timestamp
+    raw_ts = time.time()
+    # 2. Convert to a datetime object
+    dt_object = datetime.fromtimestamp(raw_ts)
+    # 3. Format it as a string
+    precise_date = dt_object.strftime("%Y-%m-%d %H:%M:%S.%f")[:-3]
+    return precise_date
+
+
+def send_command_csv(cmd: str) -> None:
+    precise_time: str = get_precise_time()
+    data: str = send_command(cmd)
+
+    file_name = f"test_{cmd}.csv"
+    file_exists = os.path.isfile(file_name)
+
+    header_map = {
+        "READ_WHOAMI": ["timestamp", "result", "command", "status", "test", "val", "raw"],
+        "READ_ACCEL": ["timestamp", "result", "command", "status", "test", "x", "y", "z", "raw"],
+        "DEFAULT": ["timestamp", "result", "command", "status", "test", "val1", "raw"]
+    }
+
+    timeout_row_map = {
+        "READ_WHOAMI": [precise_time, "FAIL", cmd, "TIMEOUT", "ERR", "N/A", "NO_DATA"],
+        "READ_ACCEL": [precise_time, "FAIL", cmd, "TIMEOUT", "ERR", "N/A", "N/A", "N/A", "NO_DATA"],
+        "DEFAULT": [precise_time, "FAIL", cmd, "TIMEOUT", "ERR", "N/A", "NO_DATA"]
+    }
+
+    with open(file_name, "a", newline="") as f:
+        writer = csv.writer(f)
+
+        if not file_exists:
+            writer.writerow(header_map.get(cmd, header_map["DEFAULT"]))
+
+        if not data:
+            print(f"Hardware Timeout: {cmd} failed.")
+            writer.writerow(timeout_row_map.get(cmd, timeout_row_map["DEFAULT"]))
+            return
+
+        separated_data: list[str] = data.split(":")
+        evaluated_res: str = evaluate_result(separated_data)
+        final_format: list[str] = [precise_time, evaluated_res, cmd] + separated_data + [data]
+
+        print(final_format)
+        writer.writerow(final_format)
+
+
+send_command_csv("READ_WHOAMI")
+send_command_csv("READ_WHOAMI")
+send_command_csv("READ_WHOAMI")
+
+send_command_csv("READ_ACCEL")
+send_command_csv("READ_ACCEL")
+send_command_csv("READ_ACCEL")
+send_command_csv("READ_ACCEL")
+
